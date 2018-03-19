f63445790e237fe93ae31c072a5e99e23333da94
Brett Schwarz <brett.schwarz@gmail.com>
2018-03-18 18:17:00 -0700

    Adds border around text, adds OUCH message and cleans up code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,40 @@
-// Enemies our player must avoid
+//
+//============================================================
+// Class: Enemy
+//   enemies our player must avoid
+//============================================================
+//
 var Enemy = function(row) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
-    //**  from engine.js
-    // width of column = 101
-    // height of row = 83
     this.row = row;
-    this.x = -cellWidth;
-    // Will add enemy in the row that is passed into this function
-    this.y = row*cellHeight - imgOffset;
+    this.x = -cellWidth; // start off screen
+    this.y = row*cellHeight - imgOffset; // find y based on the row
+    
     // use a speed function so that we can increase speed for next level
     this.speed = this.getspeed(topspeed);
 
 };
 
-// ****************************************************
-//  Speed method - get random speed, based on top speed
-// ****************************************************
+//
+//**************************************************************
+// Method:  Speed - get random speed, based on top speed
+//**************************************************************
+//
 Enemy.prototype.getspeed = function(top) {
     return Math.floor(Math.random() * top) + top - 5;
 }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//
+//**************************************************************
+// Method: Enemy.update
+//
+//  Update the enemy's position, required method for game
+//  Parameter: dt, a time delta between ticks
+//**************************************************************
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -36,6 +45,7 @@ Enemy.prototype.update = function(dt) {
         this.x = -cellWidth;
     }
 
+    // Check for collision of each enemy with the player
     if(this.x >= player.x - imgOffset && this.x <= player.x + imgOffset) {
         if(this.y >= player.y - imgOffset && this.y <= player.y + imgOffset) {
             
@@ -47,6 +57,9 @@ Enemy.prototype.update = function(dt) {
                     level -= 1;
                     topspeed -= 5;
                 }
+
+                message = "OUCH!!"
+                setTimeout(function(){message = '';}, 1000);
             } else {
                 message = "Game Over!!"
                 player.renderLives();
@@ -56,12 +69,14 @@ Enemy.prototype.update = function(dt) {
                     score = 0;
                     level = 1;
                     state = 'start';
-                    message = '';
+                    message = 'New Game';
                     lives = 3;
-                }, 3000);
+                }, 2000);
 
                 // Resets number of enemies back to three
                 allEnemies = allEnemies.slice(0,3);
+
+                setTimeout(function(){message = '';}, 3000);
             }
 
             allEnemies.forEach(function(enemy) {
@@ -72,11 +87,21 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-// Draw the enemy on the screen, required method for game
+//
+//**************************************************************
+// Method: Enemy.render
+//
+//**************************************************************
+//
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//
+//**************************************************************
+// Method: Enemy.reset
+//**************************************************************
+//
 Enemy.prototype.reset = function() {
     this.x = -cellWidth;
     this.speed = this.getspeed(topspeed);
@@ -84,10 +109,13 @@ Enemy.prototype.reset = function() {
 
 
 
-// ****************************************************
-//  Player Class
-// ****************************************************
-var Player = function(x, y) {
+
+
+//================================================
+// Class: Player
+//
+//================================================
+var Player = function() {
     this.sprite = 'images/char-boy.png';
 
     // Start at bottom row, in the middle
@@ -108,31 +136,34 @@ var Player = function(x, y) {
 //  update method
 // ****************************************************
 Player.prototype.update = function() {
-
-
+    //no-op
 }
 
 // ****************************************************
 //  render method
 // ****************************************************
 Player.prototype.render = function() {
-    ctx.font = "400 30px Gloria Hallelujah";
+
+    // Text for the Score and Level headings
+    ctx.font = "25pt Permanent Marker";
     ctx.fillStyle = 'white';
     ctx.fillText("Score: " + score, 10, ctx.canvas.height-25);
     ctx.fillText("Level: " + level, 10,85);
-    // ctx.fillText("Lives: " + lives, ctx.canvas.width-150,85);
-    // ctx.drawImage(Resources.get(this.sprite), ctx.canvas.width-100, 55, 30, 60);
+ 
+    this.renderLives();
 
-    this. renderLives();
-
+    // Text for the Game Status on screen (i.e. Game Over)
     ctx.save();
-    // ctx.font = "400 80px Gloria Hallelujah";
-    ctx.font = "60px Permanent Marker";
+    ctx.font = "50pt Permanent Marker";
     ctx.textAlign = 'center';
-    ctx.fillStyle = 'red';
+    ctx.strokeStyle = '#333';
+    ctx.lineWidth = 3;
+    ctx.fillStyle = 'blue';
     ctx.fillText(message, ctx.canvas.width/2, ctx.canvas.height/2);
+    ctx.strokeText(message, ctx.canvas.width/2, ctx.canvas.height/2);
     ctx.restore();
 
+    // Draw the player
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -141,6 +172,7 @@ Player.prototype.render = function() {
 // ****************************************************
 Player.prototype.renderLives = function() {
 
+    // Loop through each life left and render on screen
     var xoff = 35;
     for (var i = 1; i <= lives; i++) {
         ctx.drawImage(Resources.get(this.sprite), ctx.canvas.width-xoff, 35, 30, 60);
@@ -157,31 +189,36 @@ Player.prototype.handleInput = function(key) {
 
     // Handle the key press and check if the location is at an edge first
     // before moving the coords.
-    // Note: The below allows for the player to wrap around to the left 
-    // or right, once the player reaches the right or left edge.
+    // 
+    // **Note**: The below allows for the player to wrap around to the left 
+    // or right, once the player reaches the right or left edge. The Rubric
+    // mentions that player can not move off screen. If this ability violates
+    // that requirement, I can change so that it doesn't wrap. I just think
+    // wrapping allows a different dimension to the game.
+    // 
     switch (key) {
         case 'left':
             if (this.x < this.edge[key]) {
                 this.x = this.edge['right'];
             } else {
-                this.x -= 101;
+                this.x -= cellWidth;
             }
             break;
         case 'up':
             if (this.y > this.edge[key]) {
-                this.y -= 83;
+                this.y -= cellHeight;
             }
             break;
         case 'right':
             if (this.x>= this.edge[key]) {
                 this.x = 0;
             } else {
-                this.x += 101;
+                this.x += cellWidth;
             }
             break;
         case 'down':
             if (this.y < this.edge[key]) {
-                this.y += 83;
+                this.y += cellHeight;
             }
             break;
     }
@@ -189,7 +226,6 @@ Player.prototype.handleInput = function(key) {
     // check if the player has made it to the water, and
     // if so, send Won message, then reset
     if (this.y <= this.edge['up'] && state != 'won') {
-        console.log('won');
         state = 'won';
         message = 'Good Job!!';
         
@@ -197,22 +233,21 @@ Player.prototype.handleInput = function(key) {
             // score grows by level amount;
             score += level;
             level += 1;
+            // if not multiple of 5, just increase speed
+            // else if multiple of 5 then add enemy
             if (level % 5) {
                 topspeed += 5;
             } else {
                 var row = allEnemies.length % 3 + 1;
                 allEnemies.push(new Enemy(row));
-                console.log("ROW: " + row);
             }
-            // topspeed += 10;
-            // console.log(topspeed);
             player.reset();
             allEnemies.forEach(function(enemy) {
                 enemy.reset();
             });
             state = 'start';
             message = '';
-        }, 3000);
+        }, 2000);
     }
 }
 
@@ -224,35 +259,26 @@ Player.prototype.reset = function() {
     this.y = 5*cellHeight-imgOffset;
 }
 
-
-var gameReset = function() {
-
-}
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-// var allEnemies = [new Enemy(100, 300, 250), new Enemy(300, 380, 150), new Enemy(400, 220, 350), new Enemy(-700, 130, 420)];
-// var player = new Player(215, 460);
-
+// Global varaibles
 var topspeed = 180;
 var score = 0;
 var level = 1;
 var imgOffset = 25;
-var cellWidth = 101;
-var cellHeight = 83;
 var state = 'start';
 var message = '';
 var lives = 3;
+// **  from engine.js
+// width of column = 101
+// height of row = 83
+var cellWidth = 101;
+var cellHeight = 83;
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 
 var allEnemies = [new Enemy(1), new Enemy(2), new Enemy(3)]; //creates an array of Enemies
-// (function displayEnemies() {
-//     'use strict';
-//     allEnemies.push(new Enemy(1));
-//     allEnemies.push(new Enemy(2));
-//     allEnemies.push(new Enemy(3));
-// }());
-
 
 var player = new Player();
 
